6008544b1721e08e6ba16dcbbb9ec0882bf553c6
Michael <nivmichael@gmail.com>
2025-02-27 14:14:11 +0200

    Styiling and icons

--- react-micro-projects/src/App.jsx
+++ react-micro-projects/src/App.jsx
@@ -2,6 +2,7 @@ import Cart from "./components/Cart";
 import Greetings from "./components/Greetings";
 import UserStatus from "./components/UserStatus";
 import Weather from "./components/Weather";
+import './index.css'; 
 
 
 const App = () => {
--- react-micro-projects/src/components/Cart.jsx
+++ react-micro-projects/src/components/Cart.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { FaCartArrowDown } from "react-icons/fa";
+
 
 const Cart = () => {
   const items = ["item1", "item2","item3","item14"]  ;
+
   return (
     <div>
-      <h1>Cart 🛒</h1>
+      <h1 >Cart <FaCartArrowDown /></h1>
+      
       {items.length > 0 && <h2>You have {items.length} items in your Cart</h2>}
 
       <ul>
